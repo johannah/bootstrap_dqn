402f362cf66b318876b44dfba7485a2b8e03fbef
johannah <jh1736@gmail.com>
2019-10-09 12:06:23 -0400

    verified pong training

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-base_datadir = '../../dataset/'
-model_savedir = '../../model_savedir'
-results_savedir = '../../results'
+base_datadir = '../dataset/'
+model_savedir = '../model_savedir'
+results_savedir = '../results'
